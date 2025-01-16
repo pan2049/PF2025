77f427f109e4af49d6bd400843588e2d0d1b945d
pan2049 <flywingzero@gmail.com>
2025-01-17 01:17:54 +0800

    [create] sql

---
+++ pf.sql
@@ -0,0 +1,62 @@
+-- --------------------------------------------------------
+-- 主機:                           127.0.0.1
+-- 伺服器版本:                        10.11.2-MariaDB - mariadb.org binary distribution
+-- 伺服器作業系統:                      Win64
+-- HeidiSQL 版本:                  11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- 正在傾印表格  pf.access 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `access` DISABLE KEYS */;
+/*!40000 ALTER TABLE `access` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.alert_record_aio 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `alert_record_aio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alert_record_aio` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.alert_record_dio 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `alert_record_dio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alert_record_dio` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.alert_set_aio 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `alert_set_aio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alert_set_aio` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.alert_set_dio 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `alert_set_dio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alert_set_dio` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.device_info 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `device_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `device_info` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.device_type 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `device_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `device_type` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.gateway_info 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `gateway_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gateway_info` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.modbus_point 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `modbus_point` DISABLE KEYS */;
+/*!40000 ALTER TABLE `modbus_point` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.point_info 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `point_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `point_info` ENABLE KEYS */;
+
+-- 正在傾印表格  pf.site_info 的資料：~0 rows (近似值)
+/*!40000 ALTER TABLE `site_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `site_info` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
